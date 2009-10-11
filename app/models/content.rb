e4ba5c1a005db7d26cb70526f89941b08cb5b3b5
Petr Sigut <petr@sigut.net>
2009-10-11 12:34:36 +0200

    prace na Photoofthedaycom pokracuji...

--- app/models/content.rb
+++ app/models/content.rb
@@ -16,6 +16,17 @@ class Content < ActiveRecord::Base
     #(@kml_doc.add_element 'video').text = "http://tinyvid.tv/vfe/big_buck_bunny.mp4"
   end
 
+  def self.create_gallery(array_of_links)
+    html_chunk = '<div id="slideshow">'
+    html_chunk += "<img src=\"#{array_of_links[0]}\" alt=\"Slideshow Image 1\" class=\"active\" />\n"
+    array_of_links.each_with_index do |link, index|
+      html_chunk += "<img src=\"#{array_of_links[index]}\" alt=\"Slideshow Image 1\" />\n"
+    end
+
+    html_chunk += '</div>'
+
+  end
+
   def self.save_me
     # tohle až do konce můžu zoobecnit pro všechny třídy...
     @content = Content.find_by_name(self.name) # udelat pres tridni promenou?
--- app/models/photoofthedaycom.rb
+++ app/models/photoofthedaycom.rb
@@ -2,7 +2,23 @@ class Photoofthedaycom < Content
    attr_accessor :name
 
   def self.parse_raw_html
-    @doc = Hpricot(open("http://www.cocoa.de/news2/2009/03/photos/10/photo005.htm"))
+    year = Time.now.strftime("%Y")
+    # with leading zeros
+    month = Time.now.strftime("%m")
+    day = Time.now.strftime("%d")
+   
+    @doc = []
+    10.times.with_index  do |x, index|
+      if index < 9
+        @doc << "http://www.cocoa.de/news2/#{year}/#{month}/photos/#{day}/photo00#{index+1}.jpg"
+      else
+        @doc << "http://www.cocoa.de/news2/#{year}/#{month}/photos/#{day}/photo0#{index+1}.jpg"
+      end
+    end
+
+    @doc = create_gallery(@doc)
+    logger.fatal "Logujem"
+    logger.fatal @doc
 
     save_me
   end
